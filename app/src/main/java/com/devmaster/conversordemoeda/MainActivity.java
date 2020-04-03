f834561bea572b4200d105d968ebec6c92e49f1a
wenderson-me <wendrson22@gmail.com>
2020-04-03 17:00:02 -0300

    refactor

---
+++ app/src/main/java/com/devmaster/conversordemoeda/MainActivity.java
@@ -0,0 +1,19 @@
+package com.devmaster.conversordemoeda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText valor = findViewById(R.id.edit_valor);
+        valor.getText();
+
+    }
+}
--- app/src/main/java/com/devmaster/conversormoedas/MainActivity.kt
+++ app/src/main/java/com/devmaster/conversormoedas/MainActivity.kt
@@ -2,11 +2,16 @@ package com.devmaster.conversormoedas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+
+        EditText valor = findViewById(R.id.editValor);
+        valor.getText();
     }
+
 }
