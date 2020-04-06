dcdb1bee2a94ba54416d80b3ee980c3ee6002e2f
wenderson-me <wendrson22@gmail.com>
2020-04-06 15:47:33 -0300

    identificação de elementos

--- app/src/main/java/com/devmaster/conversordemoeda/MainActivity.java
+++ app/src/main/java/com/devmaster/conversordemoeda/MainActivity.java
@@ -3,17 +3,39 @@ package com.devmaster.conversordemoeda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText valor = findViewById(R.id.edit_valor);
-        valor.getText();
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.editDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.editEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
+
+        this.mViewHolder.buttonCalculate.setOnClickListener(this);
 
     }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private static class ViewHolder {
+        EditText editValue;
+        TextView editDolar;
+        TextView editEuro;
+        Button buttonCalculate;
+    }
+
 }
