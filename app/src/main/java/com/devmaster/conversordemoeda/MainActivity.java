b13ff63f5b6ee55c65f960247486815838a8dcc4
wenderson-me <wendrson22@gmail.com>
2020-04-06 17:16:50 -0300

    ñ sei oq ta acontecendo mas ta funcionando

--- app/src/main/java/com/devmaster/conversordemoeda/MainActivity.java
+++ app/src/main/java/com/devmaster/conversordemoeda/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,23 +19,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         this.mViewHolder.editValue = findViewById(R.id.edit_value);
-        this.mViewHolder.editDolar = findViewById(R.id.text_dolar);
-        this.mViewHolder.editEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.textDolar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
         this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
 
         this.mViewHolder.buttonCalculate.setOnClickListener(this);
+
+        this.clearValues();
 
     }
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.button_calculate) {
+            String value = this.mViewHolder.editValue.getText().toString();
+            if ("".equals(value)) {
+                Toast.makeText(this, this.getString(R.string.informe_um_valor), Toast.LENGTH_LONG).show();
+            } else {
+                Double real = Double.valueOf(value);
 
+                this.mViewHolder.textDolar.setText(String.format("%.2f", (real / 4)));
+                this.mViewHolder.textEuro.setText(String.format("%.2f", (real / 5)));
+            }
+        }
+    }
+
+    private void clearValues() {
+        this.mViewHolder.textDolar.setText("");
+        this.mViewHolder.textEuro.setText("");
     }
 
     private static class ViewHolder {
         EditText editValue;
-        TextView editDolar;
-        TextView editEuro;
+        TextView textDolar;
+        TextView textEuro;
         Button buttonCalculate;
     }
 
